d2726c5360046fb7a9224a78e4d8df2c6efa0fa0
ryuutar <obata229@gmail.com>
2012-11-04 21:05:19 +0900

    Up

---
+++ Polls/poll/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+     url(r'^polls/$','index'),
+     url(r'^polls/(?P<poll_id>\d+)/$','detail'),
+     url(r'^polls/(?P<poll_id>\d+)/results/$','results'),
+     url(r'^polls/(?P<poll_id>\d+)/vote/$','vote'),
+     url(r'^admin/', include(admin.site.urls)),
+)
